1493cf0f41602b32f6961da805c6cb81c5dd69dc
TheNuggitMan <thenuggitman@gmail.com>
2020-08-28 15:07:30 +0100

    InitialCommit

---
+++ playbulbCandle.js
@@ -0,0 +1,22 @@
+/* This file will contain the PlaybulbCandle Class that will be used to
+ * interact with the PLAYBULB Candle Bluetooth device. */
+(function() {
+  'use strict';
+
+  const CANDLE_SERVICE_UUID = 0xFF02;
+
+  class PlaybulbCandle {
+    constructor() {
+      this.device = null;
+    }
+    connect() {
+      let options = {filters:[{services:[CANDLE_SERVICE_UUID]}]};
+      return navigator.bluetooth.requestDevices(options)
+      .then(function(device) {
+        this.device = device;
+      }.blind(this));
+    }
+  }
+
+  window.PlaybulbCandle - new PlaybulbCandle();
+})();
